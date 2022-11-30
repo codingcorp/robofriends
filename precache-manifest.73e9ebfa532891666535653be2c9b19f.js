a7a443ad57eb0460b94fc34a6bcedd5eca5f47ed
Sacha De Vreught <sachadevreught.2302@gmail.com>
2022-12-01 00:39:14 +0100

    Updates

---
+++ precache-manifest.73e9ebfa532891666535653be2c9b19f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9c514abbdf97ed3ae6e",
+    "url": "/robofriends/static/css/main.d526d9a4.chunk.css"
+  },
+  {
+    "revision": "b9c514abbdf97ed3ae6e",
+    "url": "/robofriends/static/js/main.b9c514ab.chunk.js"
+  },
+  {
+    "revision": "4d5909f16dbfde8279fc",
+    "url": "/robofriends/static/js/1.4d5909f1.chunk.js"
+  },
+  {
+    "revision": "9948e9ebbfa0ee845b82",
+    "url": "/robofriends/static/css/2.b91eae00.chunk.css"
+  },
+  {
+    "revision": "9948e9ebbfa0ee845b82",
+    "url": "/robofriends/static/js/2.9948e9eb.chunk.js"
+  },
+  {
+    "revision": "3e9358c5e951758321bc",
+    "url": "/robofriends/static/js/runtime~main.3e9358c5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "310fee97f192d9a809c8d403a8214e21",
+    "url": "/robofriends/index.html"
+  }
+];
